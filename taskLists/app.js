e90729a6941fa6e1d665e71c7822042e6e8acbe0
olzhas2102 <olzhas2102@yahoo.com>
2018-02-17 13:52:03 +0600

    Delete and filter tasks

--- taskLists/app.js
+++ taskLists/app.js
@@ -12,6 +12,15 @@ loadEventListeners();
 function loadEventListeners() {
 	//Add task event
 	form.addEventListener('submit', addTask);
+
+	// Remove task event
+	taskList.addEventListener('click', removeTask);
+
+	// Clear tasks event
+	clearBtn.addEventListener('click', clearTasks);
+
+	// Filter tasks addEventListener
+	filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
@@ -43,4 +52,44 @@ function addTask(event) {
 	taskInput.value = '';
 
 	event.preventDefault();
+}
+
+// Remove task
+function removeTask(event) {
+	const dltIcon = event.target.parentElement.classList.contains('delete-item');
+	const confimMsg = confirm('Are you sure you want to delete the task?');
+
+	if(dltIcon) {
+		if(confimMsg) {
+			event.target.parentElement.parentElement.remove();
+		}
+	}
+}
+
+// Clear tasks
+function clearTasks() {
+	// 1 option:
+	// taskList.innerHTML = '';
+
+	// 2 option: FASTER way (https://jsperf.com/innerhtml-vs-removechild):
+	while(taskList.firstChild) {
+		taskList.removeChild(taskList.firstChild);
+	}
+}
+
+// Filter tasks
+function filterTasks(event) {
+	const text = event.target.value.toLowerCase();
+
+	document.querySelectorAll('.collection-item').forEach(function(task) {
+		const item = task.firstChild.textContent;
+
+		if(item.toLowerCase().indexOf(text) != -1) {
+			task.style.display = 'block';
+		} else {
+			task.style.display = 'none';
+		}
+	});
+
+	console.log(text);
 }
